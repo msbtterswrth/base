86ede9a00e027d0771424698ae2c303328bb0756
Customer <u784-4b4knhm45yra@gvam1310.siteground.biz>
2024-06-07 20:19:25 +0000

    Revert "Init homepage content and update theme branding."
    This reverts commit 2c116c6bfdac844a40720e855969b5425ae9fea7.

--- web/modules/custom/node_block/src/Plugin/Block/NodeBlock.php
+++ web/modules/custom/node_block/src/Plugin/Block/NodeBlock.php
@@ -9,11 +9,10 @@ use Drupal\Core\Entity\EntityViewBuilderInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\node\Entity\Node;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a block for nodes.
- *
  * @Block(
  *   id = "node_block",
  *   admin_label = @Translation("Node Block")
@@ -21,69 +20,41 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class NodeBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
-  /**
-   * The field manager interface.
-   *
-   * @var \Drupal\Core\Entity\EntityFieldManagerInterface*/
+  /** @var EntityFieldManagerInterface $entityManager */
   protected $entityManager;
 
-  /**
-   * The entity view build interface.
-   *
-   * @var \Drupal\Core\Entity\EntityViewBuilderInterface
-   */
+  /** @var EntityViewBuilderInterface $viewBuilder */
   protected $viewBuilder;
 
-  /**
-   * The view mode.
-   *
-   * @var string
-   */
-  protected $viewMode = 'block';
+  protected $view_mode = 'block';
 
-  /**
-   * The node.
-   *
-   * @var \Drupal\node\Entity\NodeInterface
-   */
-  protected $node = NULL;
-
-  /**
-   * {@inheritdoc}
-   */
   public function __construct(EntityFieldManagerInterface $entityManager, EntityViewBuilderInterface $viewBuilder, array $configuration, $plugin_id, $plugin_definition) {
     $this->entityManager = $entityManager;
     $this->viewBuilder = $viewBuilder;
     parent::__construct($configuration, $plugin_id, $plugin_definition);
   }
 
-  /**
-   * {@inheritdoc}
-   */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $entityManager = \Drupal::service('entity_field.manager');
     $viewBuilder = \Drupal::entityTypeManager()->getViewBuilder('node');
     return new static($entityManager, $viewBuilder, $configuration, $plugin_id, $plugin_definition);
   }
 
-  /**
-   * Build the block.
-   */
   public function build() {
     $request = \Drupal::request();
-    $this->node = $request->attributes->get('node');
-    if ($this->node) {
-      if (!is_object($this->node)) {
-        $this->node = Node::load($this->node);
+    $node = $request->attributes->get('node');
+    if ($node) {
+      if (!is_object($node)) {
+        $node = Node::load($node);
       }
     }
 
     $build = [];
-    if ($this->node) {
-      $fields = $this->entityManager->getFieldDefinitions('node', $this->node->bundle());
+    if ($node) {
+      $fields = $this->entityManager->getFieldDefinitions('node', $node->bundle());
       foreach ($fields as $name => $definition) {
-        if (!$this->node->get($name)->isEmpty()) {
-          $build[$name] = $this->viewBuilder->viewField($this->node->get($name), $this->viewMode);
+        if (!$node->get($name)->isEmpty()) {
+          $build[$name] = $this->viewBuilder->viewField($node->get($name), $this->view_mode);
         }
       }
     }
@@ -91,18 +62,12 @@ class NodeBlock extends BlockBase implements ContainerFactoryPluginInterface {
     return $build;
   }
 
-  /**
-   * Clear the default theme configuration.
-   */
   public function defaultConfiguration() {
     return [
       'theme_suggestion' => '',
     ];
   }
 
-  /**
-   * Create the array of theme suggestions.
-   */
   public function blockForm($form, FormStateInterface $form_state) {
     return [
       'theme_suggestion' => [
@@ -114,15 +79,12 @@ class NodeBlock extends BlockBase implements ContainerFactoryPluginInterface {
     ];
   }
 
-  /**
-   * Provide the theme suggestions.
-   */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['theme_suggestion'] = $form_state->getValue('theme_suggestion');
   }
 
   /**
-   * {@inheritdoc}
+   * @return \string[]
    */
   public function getCacheContexts() {
     return Cache::mergeContexts([
@@ -131,15 +93,15 @@ class NodeBlock extends BlockBase implements ContainerFactoryPluginInterface {
   }
 
   /**
-   * {@inheritdoc}
+   * @return array|\string[]
    */
-  public function getCacheTags() {
-    if (!$this->node) {
-      return parent::getCacheTags();
-    }
-    return Cache::mergeTags([
-      'node:' . $this->node->id(),
-    ], parent::getCacheTags());
-  }
+  // public function getCacheTags() {
+  //   if (!$this->node) {
+  //     return parent::getCacheTags();
+  //   }
+  //   return Cache::mergeTags([
+  //     'node:' . $this->node->id(),
+  //   ], parent::getCacheTags());
+  // }
 
 }
